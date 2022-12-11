546b6b06e66dc446fa3f991f71216cf9becd4a3b
jovkin27 <118188660+jovkin27@users.noreply.github.com>
2022-12-11 16:30:43 +0200

    Добавьте файлы проекта.

---
+++ Math/Math.py
@@ -0,0 +1,96 @@
+﻿from random import*
+
+print('Valige raskusaste')
+print('Tase 1=>"1" Tase 2=>"2" Tase 1extra=>"3" Tase 2extra=>"4" ')
+while True:
+    tase=int(input(' '))
+    if tase==1:
+        j=0
+        for i in range(1,11):
+            arv_1=randint(-10,10)
+            arv_2=randint(-10,10)
+            i=(f'Mis on {arv_1}+{arv_2}?')
+            vastus=arv_1+arv_2
+            print(i)
+            user_vastus=int(input('vastus=>'))
+            if vastus==user_vastus: 
+                j+=10
+                print('Õige')
+            else:
+                print('Vale')
+        if j<=60:
+            print(f'Te saate {j}% Hinne on 2')
+        elif j>=61 and j<75:
+            print(f'Te saate {j}% Hinne on 3')
+        elif j>=75 and j<=90:
+            print(f'Te saate {j}% Hinne on 4')
+        else:
+            print(f'Te saate {j}% Hinne on 5')
+    elif tase==2:
+        j=0
+        for i in range(1,11):
+            arv_1=randint(-10,10)
+            arv_2=randint(-10,10)
+            i=(f'Mis on {arv_1}*{arv_2}?')
+            vastus=arv_1*arv_2
+            print(i)
+            user_vastus=int(input('vastus=>'))
+            if vastus==user_vastus: 
+                j+=10
+                print('Õige')
+            else:
+                print('Vale')
+        if j<=60:
+            print(f'Te saate {j}% Hinne on 2')
+        elif j>=61 and j<75:
+            print(f'Te saate {j}% Hinne on 3')
+        elif j>=75 and j<=90:
+            print(f'Te saate {j}% Hinne on 4')
+        else:
+            print(f'Te saate {j}% Hinne on 5')
+    elif tase==3:
+        j=0
+        for i in range(1,11):
+            arv_1=randint(-100,100)
+            arv_2=randint(-100,100)
+            i=(f'Mis on {arv_1}+{arv_2}?')
+            vastus=arv_1+arv_2
+            print(i)
+            user_vastus=int(input('vastus=>'))
+            if vastus==user_vastus:
+                j+=10   
+                print('Õige')
+            else:
+                print('Vale')
+        if j<=60:
+            print(f'Te saate {j}% Hinne on 2')
+        elif j>=61 and j<75:
+            print(f'Te saate {j}% Hinne on 3')
+        elif j>=75 and j<=90:
+            print(f'Te saate {j}% Hinne on 4')
+        else:
+            print(f'Te saate {j}% Hinne on 5')
+    elif tase==4:
+        j=0
+        for i in range(1,11):
+            arv_1=randint(1,10)
+            arv_2=randint(1,3)
+            c=(f'Mis on {arv_1}**{arv_2}?')
+            vastus=arv_1**arv_2
+            print(i)
+            user_vastus=int(input('vastus=>'))
+            if vastus==user_vastus:
+                j+=10
+                print('Õige')
+            else:
+                print('Vale')
+        if j<=60:
+            print(f'Te saate {j}% Hinne on 2')
+        elif j>=61 and j<75:
+            print(f'Te saate {j}% Hinne on 3')
+        elif j>=75 and j<=90:
+            print(f'Te saate {j}% Hinne on 4')
+        else:
+            print(f'Te saate {j}% Hinne on 5')
+    if tase>0 and tase<5: break
+    print('Te saate valida ainult 1,2,3 või 4')
